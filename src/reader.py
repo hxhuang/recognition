c4949ccafc5618e36d42163eed3e4d33c17a32a9
zeaker <hxhuangnet@163.com>
2018-12-07 15:13:36 +0800

    https://github.com/hxhuang/recognition

--- src/reader.py
+++ src/reader.py
@@ -16,21 +16,23 @@ class Reader:
     @classmethod
     def getMoney(cls, data):
         tesseract_cmd = "/usr/local/Cellar/tesseract/4.0.0"
+        # tesseract_cmd = "/usr"
         _path = path.dirname(__file__)
         _path = path.join(_path, "..", "caches", str(uuid.uuid4()) + ".png")
         _money = "0.00"
 
-        with open(_path, "wb") as up:
-            up.write(data['body'])
-
-
+        try:
+            with open(_path, "wb") as up:
+                up.write(data['body'])
+        except Exception as error:
+            print str(error)
 
         try:
-            _image = pytesseract.image_to_string(_path, config="--psm 9 sfz")
+            _image = pytesseract.image_to_string(_path, config="-psm 9 sfz")
             if _image:
                 _money = _image.replace("_", "")
         except Exception as error:
-            pass
+            print str(error)
 
         remove(_path)
         return _money
